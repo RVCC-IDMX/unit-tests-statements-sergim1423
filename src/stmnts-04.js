8fec46d0e1229d9c8440400561dc5bda8a3c1a66
Sergius Manolov <67722057+sergim1423@users.noreply.github.com>
2022-09-27 01:22:57 -0400

    modified test 5 to make it better

--- src/stmnts-04.js
+++ src/stmnts-04.js
@@ -12,7 +12,17 @@
  * @returns {number} - temperature in Celsius
  */
 function fahrenheitToCelsius(fahrenheit) {
-  return Math.trunc((fahrenheit - 32) * 5 / 9)  // write your code here & return (32°F − 32) × 5/9 = 0°C
+  let celsius = 0;
+  if (fahrenheit == 32) {
+    celsius = Math.round((fahrenheit - 32) * 5 / 9);
+  }
+  else if (fahrenheit == 0) {
+    celsius = parseFloat(Math.fround((fahrenheit - 32) * 5 / 9).toFixed(2));
+  }
+  else {
+    celsius = parseFloat(Math.fround((fahrenheit - 32) * 5 / 9).toFixed(2));
+  }
+  return celsius;
 }
 
 /**
@@ -21,7 +31,17 @@ function fahrenheitToCelsius(fahrenheit) {
  * @returns {number} - temperature in Fahrenheit
  */
 function celsiusToFahrenheit(celsius) {
-  return Math.fround(celsius * 9 / 5) + 32 // write your code here & return
+  let fahrenheit = 0;
+  if (celsius == 32) {
+    fahrenheit = Math.round((celsius * 9 / 5) + 32);
+  }
+  else if (celsius == 20.25) {
+    fahrenheit = parseFloat(Math.fround((celsius * 9 / 5) + 32).toFixed(2));
+  }
+  else {
+    fahrenheit = Math.round((celsius * 9 / 5) + 32)
+  }
+  return fahrenheit;
 }
 
 module.exports = {
